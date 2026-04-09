94500ecf619d193142db37b6bd49292db198b883
montesnahuel2001-max <montesnahuel2001@gmail.com>
2026-04-08 21:50:13 -0300

    Delete sw (2).js

--- sw (2).js
+++
@@ -1,4 +0,0 @@
-const CACHE = 'mantenipro-v5';
-const ASSETS = ['/', '/index.html', '/scanner.html'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html')))));
